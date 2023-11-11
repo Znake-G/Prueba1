94399ab27bfb8d66cd37ffab2d77e89227d093f2
Jose Manuel <111933458+Znake-G@users.noreply.github.com>
2023-11-11 16:00:50 -0500

    create core file

---
+++ core/core.py
@@ -0,0 +1,6 @@
+from datetime import date, timedelta
+from typing import List
+
+from person.persona import Cliente, Instructor
+from product.producto import Curso, Plan
+from product.relacion import PlanCliente, ProductoCliente
